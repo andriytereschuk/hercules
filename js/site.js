6195c386a7eed6159904ab9cb138286ab87bdbe7
andriytereschuk <shatskyat@gmail.com>
2015-10-29 10:32:18 +0200

    responsive: header,about, gallery, participate

--- js/site.js
+++ js/site.js
@@ -1,65 +1,137 @@
 $(function(){
-	// Gallery slider
-	var swiper2 = new Swiper('#gallery', {
-	    slidesPerView: 'auto',
-	    resistanceRatio: 0
+	// top slider
+
+	var swiper1 = new Swiper('#topSlider', {
+	    loop: true,
+	    autoplay: false,
+	    //autoplay: 4000,
+	    speed: 600
 	});
 
-	// Gallery slider arrows
+	// Gallery slider
+
 	var $slider = $('.gallery-slider');
 	var $slidePrev = $('.slidePrev');
 	var $slideNext = $('.slideNext');
 
+	var swiper2 = new Swiper('#gallerySlider', {
+	    slidesPerView: 'auto',
+	    centeredSlides: true,
+	    loop: true
+	});
+
 	$slidePrev.on('click', function(){
 		swiper2.slidePrev();
-		checkBeginEndSlider();
 	});
 
 	$slideNext.on('click', function(){
 		swiper2.slideNext();
-		checkBeginEndSlider();
 	});
-
-	function checkBeginEndSlider() {
-		if(swiper2.isBeginning) {
-			$slidePrev.addClass('hide');
-			$slider.addClass('hide-before');
-		}
-		else {
-			$slidePrev.removeClass('hide');
-			$slider.removeClass('hide-before');
-		}
-
-		if(swiper2.isEnd) {
-			$slideNext.addClass('hide');
-			$slider.addClass('hide-after');
-		}
-		else {
-			$slideNext.removeClass('hide');
-			$slider.removeClass('hide-after');
-		}
-	}
-
-	if(swiper2.isBeginning) console.log('isBeginning');
-
 
 	// Scroll animation for Gallery
-	var anim = true;
+	var centeredSlide = Math.floor(swiper2.slides.length/2) - 2;
+	swiper2.slideTo(centeredSlide, 0);
 
-	$(window).scroll(function(){
-		var galleryTop = $("#gallery").offset().top;
-		
+	// Nav
 
-	  if($(window).scrollTop() + $(window).height() > galleryTop && anim)
-	  {
-			var centeredSlide = Math.floor(swiper2.slides.length/2) - 2;
-			setTimeout(function(){
-				swiper2.slideTo(centeredSlide, 1000);
-			}, 100);
-			anim = false;
-	  }
+	$('header').onePageNav({
+		'scrollSpeed': 600
 	});
 
-	// Gallery item hover effect
-	$(' #gallery .swiper-slide a').each( function() { $(this).hoverdir(); } );
+	// header video LT
+
+	$('#playVideo1').on('click', function(event){
+		event.preventDefault();
+		var videoSrc = 'https://www.youtube.com/embed/B3Dubc--Wik?rel=0&amp;showinfo=0&autoplay=1';
+
+		$('.top-video').addClass('open');
+		$('#video1').attr('src', videoSrc);
+		swiper1.stopAutoplay();
+	});
+
+	$('#closeVideo1').on('click', function(event){
+		event.preventDefault();
+
+		$('.top-video').removeClass('open');
+		$('#video1').attr('src', '');
+		swiper1.startAutoplay();
+	});
+
+	// participate video LT
+
+	$('#playVideo2').on('click', function(event){
+		event.preventDefault();
+		var videoSrc = 'https://www.youtube.com/embed/xsmSyoPv5_w?rel=0&amp;showinfo=0&autoplay=1';
+
+		$('.fixed-video').addClass('open');
+		$('#video2').attr('src', videoSrc);
+	});
+
+	$('#closeVideo2').on('click', function(event){
+		event.preventDefault();
+
+		$('.fixed-video').removeClass('open');
+		$('#video2').attr('src', '');
+	});
+	
+	// header video EN
+
+	$('#playVideo3').on('click', function(event){
+		event.preventDefault();
+		var videoSrc = 'https://www.youtube.com/embed/9BFA7fRTzfI?rel=0&amp;showinfo=0&autoplay=1';
+
+		$('.top-video').addClass('open');
+		$('#video3').attr('src', videoSrc);
+		swiper1.stopAutoplay();
+	});
+
+	$('#closeVideo3').on('click', function(event){
+		event.preventDefault();
+
+		$('.top-video').removeClass('open');
+		$('#video3').attr('src', '');
+		swiper1.startAutoplay();
+	});
+
+	// participate video EN
+
+	$('#playVideo4').on('click', function(event){
+		event.preventDefault();
+		var videoSrc = 'https://www.youtube.com/embed/TsYDoxL0wc4?rel=0&amp;showinfo=0&autoplay=1';
+
+		$('.fixed-video').addClass('open');
+		$('#video4').attr('src', videoSrc);
+	});
+
+	$('#closeVideo4').on('click', function(event){
+		event.preventDefault();
+
+		$('.fixed-video').removeClass('open');
+		$('#video4').attr('src', '');
+	});
+	
+
+	// lightbox
+
+	lightbox.option({
+    'showImageNumberLabel': false,
+    'wrapAround': true
+  });
+
+  // footer tabs
+
+  $('.contacts-tab').on('click', function(){
+  	var self = $(this);
+  	var id = self.attr('id');
+
+  	if(self.hasClass('active')) return;
+
+  	$('.contacts-tab').removeClass('active');
+  	$('.contacts-tab__content').removeClass('active');
+
+  	$('#'+id).addClass('active');
+  	$('div[data-tab="'+ id +'"]').addClass('active');
+  });
+
+
 });
